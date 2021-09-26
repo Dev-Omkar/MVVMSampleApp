e4c94681de4541c2fd41d2e1712f005ddf45d9c7
omkar.kadam <omkarkadam.freelancer@gmail.com>
2021-09-26 18:36:39 +0530

    Posts storyboard added with tabviewcontroller

---
+++ MediSampleApp/View/PostsListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PostsListViewController.swift
+//  MediSampleApp
+//
+//  Created by MacStar on 26/09/21.
+//
+import UIKit
+
+class PostsListViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
