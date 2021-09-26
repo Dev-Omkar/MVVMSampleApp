c749dd69633334f08a712a6a7ffa0144573d1e7b
omkar.kadam <omkarkadam.freelancer@gmail.com>
2021-09-26 17:56:55 +0530

    Api service for login added

---
+++ MediSampleApp/Utilities/LoginManager.swift
@@ -0,0 +1,15 @@
+//
+//  LoginManager.swift
+//  MediSampleApp
+//
+//  Created by MacStar on 26/09/21.
+//
+
+import Foundation
+
+class LoginApiService{
+    func performLogin(with username: String,and password: String){
+        
+    }
+}
+
