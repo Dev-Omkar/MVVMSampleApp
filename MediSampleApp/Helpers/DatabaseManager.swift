91a5c4f81456e400d23569ed51990b309c5fd416
omkar.kadam <omkarkadam.freelancer@gmail.com>
2021-09-27 20:07:39 +0530

    Model changes, closure added

--- MediSampleApp/Helpers/DatabaseManager.swift
+++ MediSampleApp/Helpers/DatabaseManager.swift
@@ -11,15 +11,21 @@ class DatabaseManager {
     public static let shared = DatabaseManager()
     let entityName = "Post"
     
-    func insertData(postModel:PostModel){
+    func insertData(postModel:PostDataModel){
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         let managedContext = appDelegate.persistentContainer.viewContext
         guard let userEntity = NSEntityDescription.entity(forEntityName: entityName, in: managedContext) else { return  }
         let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
-        user.setValue(postModel.id, forKeyPath: "id")
+        user.setValue(postModel.id.stringValue, forKeyPath: "id")
         user.setValue(postModel.title, forKey: "title")
         user.setValue(postModel.body, forKey: "body")
-        user.setValue(postModel.userId, forKey: "userId")
+        user.setValue(postModel.userId.stringValue, forKey: "userId")
+        if let exist = getPostRecord(id: postModel.id){
+            user.setValue(exist.isFavourite.intValue.stringValue, forKey: "isFavourite")
+        }else{
+            user.setValue("0", forKey: "isFavourite")
+        }
+        
         do {
             try managedContext.save()
         } catch let error as NSError {
@@ -40,21 +46,21 @@ class DatabaseManager {
         }
     }
     
-    func retrieveData() -> [PostModel] {
-        var postModels : [PostModel] = [PostModel]()
+    func retrieveData() -> [PostDataModel] {
+        var postModels : [PostDataModel] = [PostDataModel]()
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return postModels}
         let managedContext = appDelegate.persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
         do {
             let result = try managedContext.fetch(fetchRequest)
-            var postModel = PostModel.init()
+            var postModel = PostDataModel.init()
             for data in result as! [NSManagedObject] {
-                postModel = PostModel.init()
-                postModel.id = data.value(forKey: "id") as? Int ?? 0
+                postModel = PostDataModel.init()
+                postModel.id = Int(data.value(forKey: "id") as? String ?? "0") ?? 0
                 postModel.title = data.value(forKey: "title") as? String ?? ""
                 postModel.body = data.value(forKey: "body") as? String ?? ""
-                postModel.userId = data.value(forKey: "userId") as? Int ?? 0
-                
+                postModel.userId = Int(data.value(forKey: "userId") as? String  ?? "0") ?? 0
+                postModel.isFavourite =  Int(data.value(forKey: "isFavourite") as? String  ?? "0")?.boolValue ?? false
                 postModels.append(postModel)
                 
             }
@@ -64,6 +70,82 @@ class DatabaseManager {
         return postModels
     }
     
+    
+    func retrieveFavData() -> [PostDataModel] {
+        var postModels : [PostDataModel] = [PostDataModel]()
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return postModels}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: "isFavourite == %@","1")
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            var postModel = PostDataModel.init()
+            for data in result as! [NSManagedObject] {
+                postModel = PostDataModel.init()
+                postModel.id = Int(data.value(forKey: "id") as? String ?? "0") ?? 0
+                postModel.title = data.value(forKey: "title") as? String ?? ""
+                postModel.body = data.value(forKey: "body") as? String ?? ""
+                postModel.userId = Int(data.value(forKey: "userId") as? String  ?? "0") ?? 0
+                postModel.isFavourite =  Int(data.value(forKey: "isFavourite") as? String  ?? "0")?.boolValue ?? false
+                postModels.append(postModel)
+                
+            }
+        } catch {
+            print("Failed")
+        }
+        return postModels
+    }
+    
+    func getPostRecord(id:Int) -> PostDataModel? {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        
+        fetchRequest.predicate = NSPredicate(format: "id == %@","\(id)")
+        
+        do {
+            let result = try context.fetch(fetchRequest)
+            var postModel = PostDataModel.init()
+            for data in result as! [NSManagedObject] {
+                postModel = PostDataModel.init()
+                postModel.id = Int(data.value(forKey: "id") as? String ?? "0") ?? 0
+                postModel.title = data.value(forKey: "title") as? String ?? ""
+                postModel.body = data.value(forKey: "body") as? String ?? ""
+                postModel.userId = Int(data.value(forKey: "userId") as? String  ?? "0") ?? 0
+                postModel.isFavourite =  Int(data.value(forKey: "isFavourite") as? String  ?? "0")?.boolValue ?? false
+                return postModel
+            }
+        } catch {
+            print("Failed")
+        }
+        return nil
+    }
+    func updateFavourite(isFav: Bool,id:Int) {
+        let value = isFav.intValue.stringValue
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        
+        fetchRequest.predicate = NSPredicate(format: "id == %@","\(id)")
+        
+        do {
+            let results = try context.fetch(fetchRequest) as? [NSManagedObject]
+            if results?.count != 0 { // Atleast one was returned 
+                // In my case, I only updated the first item in results
+                results?[0].setValue(value, forKey: "isFavourite")
+            }
+        } catch {
+            print("Fetch Failed: \(error)")
+        }
+        
+        do {
+            try context.save()
+        }
+        catch {
+            print("Saving Core Data Failed: \(error)")
+        }
+    }
     
 }
 
--- MediSampleApp/Helpers/RepositoryManager.swift
+++ MediSampleApp/Helpers/RepositoryManager.swift
@@ -7,15 +7,44 @@
 
 import Foundation
 class RepositoryManager {
-    static var sharedInstance = ApiService.init()
-    func getPosts(completion:@escaping (Result<[PostModel], URLError>) -> Void) {
+    static var sharedInstance = RepositoryManager.init()
+    typealias complete = (Result<[PostDataModel], URLError>) -> Void
+    func getPosts(completion:@escaping complete) {
         let rechability = RechabilityManager.shared
         if rechability.isReachable{
             ApiService.sharedInstance.getPosts() { response in
-                completion(response)
+                switch(response){
+                case .success(let models):
+                    var empData = [PostDataModel]()
+                    empData.removeAll()
+                    for mode in models{
+                        var dataModel = PostDataModel.init()
+                        dataModel.body = mode.body
+                        dataModel.title = mode.title
+                        dataModel.id = mode.id
+                        dataModel.userId = mode.userId
+                        dataModel.isFavourite = false
+                        empData.append(dataModel)
+                    }
+                    self.doDatabaseOperations(empData: empData)
+                    completion(.success(empData))
+                case .failure(let error):  
+                    completion(.failure(error))
+                }
             }
         }else{
             return completion(.success(DatabaseManager.shared.retrieveData()))
+        }
+    }
+    
+    func doDatabaseOperations(empData:[PostDataModel]){
+        DispatchQueue.main.async {
+            DatabaseManager.shared.deleteAll()
+            for post in empData{
+                DatabaseManager.shared.insertData(postModel: post)
+            }
+            let newPost = DatabaseManager.shared.retrieveData()
+            print("inserted posts: \(newPost.count )")
         }
     }
 }
