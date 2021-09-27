eeaf5fed57adbfa235f61085502945fcfe301d6f
omkar.kadam <omkarkadam.freelancer@gmail.com>
2021-09-27 20:08:16 +0530

    addd search functionality,logout, selection changes

--- MediSampleApp/View/PostsListViewController.swift
+++ MediSampleApp/View/PostsListViewController.swift
@@ -5,37 +5,73 @@
 //  Created by MacStar on 26/09/21.
 //
 import UIKit
- 
-class PostsListViewController: UIViewController {
+
+class PostsListViewController: UIViewController,UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        employeeViewModel.filter(seachValue: searchText)
+    }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        employeeViewModel.filter(seachValue: searchBar.text ?? "")
+        searchBar.resignFirstResponder()
+    }
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    @IBAction func logoutAction(_ sender: Any) { 
+        employeeViewModel.performLogout { (response) in
+            switch(response){
+            case .success(let responseModel):
+                if responseModel.success{
+                    self.navigationController?.popToRootViewController(animated: true)
+                }else{
+                    //show message
+                }
+                break
+            case .failure(let _):
+                //show message
+                break
+            }
+        }
+    }
     @IBOutlet weak var employeeTableView: UITableView!
     
     private var employeeViewModel : EmployeesViewModel!
     
-    private var dataSource : EmployeeTableViewDataSource<PostViewCell,PostModel>!
-    
+    private var dataSource : EmployeeTableViewDataSource<PostViewCell,PostDataModel>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        callToViewModelForUIUpdate()
-    }
-    
-    func callToViewModelForUIUpdate(){
-        self.employeeTableView.register(UINib(nibName: "PostViewCell", bundle: nil), forCellReuseIdentifier: "PostViewCell")
-        
+        self.searchBar.delegate = self
+        self.employeeTableView.tableFooterView = UIView.init()
         self.employeeViewModel =  EmployeesViewModel()
+        employeeViewModel.callFuncToGetEmpData()
+        callToViewModelForUIUpdate() 
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        employeeViewModel.reloadFromDb()
+    }
+    func callToViewModelForUIUpdate(){
+        employeeTableView.keyboardDismissMode = .onDrag
+        
+        self.employeeTableView.register(UINib(nibName: "PostViewCell", bundle: nil), forCellReuseIdentifier: "PostViewCell")
+        self.employeeTableView.delegate = self
+        
         self.employeeViewModel.bindEmployeeViewModelToController = {
             self.updateDataSource()
         }
+        
     }
     
     func updateDataSource(){
         
         self.dataSource = EmployeeTableViewDataSource(cellIdentifier: "PostViewCell", items: self.employeeViewModel.empData, configureCell: { (cell, evm) in
-            //     cell.titleTextLabel.text = "1"
             cell.titleTextLabel.text = evm.title
             cell.bodyTextLabel.text = evm.body
+            if evm.isFavourite{
+                cell.favImage.setImage(UIImage.init(named: "like"), for: .normal)
+            }else{
+                cell.favImage.setImage(UIImage.init(named: "dislike"), for: .normal)
+            } 
         })
         
         DispatchQueue.main.async {
@@ -44,9 +80,17 @@ class PostsListViewController: UIViewController {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
-
+extension PostsListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.employeeViewModel.addToFavourite(indexNumber: indexPath.row)
+    }
+}
 
 class EmployeeTableViewDataSource<Cell :PostViewCell,ViewModel> : NSObject, UITableViewDataSource {
     
@@ -55,7 +99,6 @@ class EmployeeTableViewDataSource<Cell :PostViewCell,ViewModel> : NSObject, UITa
     var configureCell :(Cell,ViewModel) -> ()
     
     init(cellIdentifier :String, items :[ViewModel], configureCell: @escaping (Cell,ViewModel) -> ()) {
-        
         self.cellIdentifier = cellIdentifier
         self.items = items
         self.configureCell = configureCell
@@ -66,11 +109,11 @@ class EmployeeTableViewDataSource<Cell :PostViewCell,ViewModel> : NSObject, UITa
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! Cell
         let item = self.items[indexPath.row]
         self.configureCell(cell,item)
         return cell
     }
+    
     
 }
